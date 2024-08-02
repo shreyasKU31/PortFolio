75bb1a98e2f9e60d470c8058c3566c983506fef5
Shreyas-KU <4si22ei005@sit.ac.in>
2024-08-02 19:48:35 +0530

    portfolio

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+
+let size = window.innerWidth;
+let ham = document.querySelector('.fas');
+let resl = document.querySelector('.resul');
+
+
+ham.addEventListener('click',() => {
+    if(ham.className == "fas fa-bars"){
+        resl.style.transform = "none";
+        ham.className = "fa-solid fa-xmark";
+    }
+    else if(ham.className == "fa-solid fa-xmark"){
+        resl.style.transform = "scale(0)";
+        ham.className = "fas fa-bars";
+    }
+});
+    
